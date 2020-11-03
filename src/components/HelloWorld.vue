45af095b7541f5e055d4d30b4b85650b0783419e
hexome <northloi@163.com>
2020-11-03 16:10:19 +0800

    vue2 refs method

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
-    <input type='text' v-model='num1'>
+    <input type='text' ref='num1' @keyup='add()'>
     <span>+</span>
-    <input type='text' v-model='num2'>
+    <input type='text' ref='num2' @keyup='add()'>
     <span>=</span>
     {{result}}
   </div>
@@ -17,17 +17,20 @@ export default {
   data () {
     return {
       num1: 0, 
-      num2: 0
+      num2: 0,
+      result: 0
     }
   },
-  computed: {
-    result(){
-      return parseInt(this.num1) + parseInt(this.num2)
-    }
+  // computed: {
+  //   result(){
+  //     return parseInt(this.num1) + parseInt(this.num2)
+  //   }
+  // },
+  methods: {
+    add(){
+      this.result = parseInt(this.$refs.num1.value) + parseInt(this.$refs.num2.value)
+    } 
   }
-  // methods: {
-  //   handelClick: 
-  // }
 }
 </script>
 

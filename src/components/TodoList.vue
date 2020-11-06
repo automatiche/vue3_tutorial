602727c6c596b9afedddc11fb9dddf988127a4bd
hexome <northloi@163.com>
2020-11-06 17:21:20 +0800

    添加的方法

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,11 @@
 <template>
   <div class='container'>
     <h1>todolist</h1>
+    <h3>共有<span class='text-primary'>{{lists.length}}</span>个任务,其中<span class='text-success'>{{finish.length}}</span>个已完成任务</h3>
     <h3>未完成的列表</h3>
     <ul>
       <template  v-for='(item, index) in lists'>
+        <!-- 此处点击click修改checked状态的行为是否不推荐 -->
         <li class="list-group-item" :key='index' @click='()=>(item.checked = !item.checked)' v-if='!item.checked'>
           <div class="form-group form-check">
             <input type="checkbox" class="form-check-input" :id="'item-' + index"
@@ -26,6 +28,19 @@
         </div>
       </li>
     </ul>
+    <h3>添加新的task</h3>
+    <div class="form-group">
+      <label for="add">添加</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="add" 
+        placeholder="添加新的tesk" 
+        v-model='value'
+        @keydown.enter='add'
+      >
+    </div>
+    <button type="button" @click='add' class="btn btn-primary btn-lg btn-block">确定添加</button>
   </div>
 </template>
 
@@ -40,7 +55,17 @@ export default {
     // 3 添加item  -> item数据结构 -> name, checked , isEdit
     // 4 双击进行编辑
     // 5 删除功能 -> 删除特定index的元素
+
+    const add = () => {
+      state.lists.push({
+        name: state.value,
+        checked: false,
+        isEdit: false
+      })
+      state.value = ''
+    }
     const state = reactive({
+      value: '',
       lists: [
         {
           name: '1',
@@ -57,7 +82,8 @@ export default {
           isEdit: false
         },
       ],
-      finish: computed(() => state.lists.filter((item)=>item.checked == true))
+      finish: computed(() => state.lists.filter((item)=>item.checked == true)),
+      add,
     })
     return toRefs(state)
   }
